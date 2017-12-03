bf31b8b35b98fe06dac2e4e26a40d1742e81a529
Lee Yiyuan <leeyiyuan@localhost.localdomain>
2017-12-03 12:07:30 +0800

    Changed representation of sparse bit array to std::bitset.

--- 136.cpp
+++ 136.cpp
@@ -1,30 +1,46 @@
 /*
     Similar to #135.
+
+    Here we use a bitset instead of an unordered_set because we need not track
+    count but only check for duplicates. A bitset uses less memory for
+    representing sparse bit arrays, and is faster than an unordered_set because
+    key-value pairs need not be created on the fly.
 */
 
 #include <iostream>
-#include <unordered_map>
+#include <bitset>
 
 int main()
 {
-    int n_max = 50000000;
-    std::unordered_map<int, int> solution_counts;
+    int const n_max = 50000000;
+
+     // Allocate on heap to prevent segfault from insufficient stack space.
+    std::bitset<n_max> *unique = new std::bitset<n_max>();
+    std::bitset<n_max> *duplicate = new std::bitset<n_max>();
+
     for (int y = 0; y < n_max; y++)
     {
         int x = y * 5 / 4 + 1;
         int n = y * (4 * x - 5 * y);
         while (n < n_max && x < 2 * y)
         {
-            solution_counts[n]++;
+            //std::cout << n << std::endl << std::flush;
+            if ((*unique)[n])
+            {
+                (*unique)[n] = false;
+                (*duplicate)[n] = true;
+            }
+            else if (!(*duplicate)[n])
+            {
+                (*unique)[n] = true;
+            }
+
             x++;
             n += 4 * y; // Because 4(x + 1)y - 5y^2 = (4xy - 5y^2) + 4y
         }
     }
 
-    int count = 0;
-    for (auto &pair : solution_counts)
-        if (pair.second == 1)
-            count++;
+    std::cout << unique->count();
 
-    std::cout << count;
+    delete unique, duplicate;
 }
