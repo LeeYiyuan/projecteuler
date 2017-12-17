c1a27f7e7ced029949cafe4760fdba87601e53f9
LeeYiyuan <leeyiyuan@hotmail.com>
2017-12-17 21:50:04 +0800

    Tweaks.

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ devnull = open(os.devnull, 'w')
 def check_jobs(value):
     ivalue = int(value)
     if ivalue <= 0:
-         raise argparse.ArgumentTypeError("Invalid argument for JOBS: %s." % value)
+         raise argparse.ArgumentTypeError('Invalid argument for JOBS: {0}.'.format(value))
     return ivalue
 
 def check_solution(value):
@@ -22,10 +22,10 @@ def check_solution(value):
         return 'ALL';
     ivalue = int(value)
     if ivalue <= 0:
-         raise argparse.ArgumentTypeError("Invalid argument for SOLUTION: %s." % value)
+         raise argparse.ArgumentTypeError('Invalid argument for SOLUTION: {0}.'.format(value))
     return value
 
-parser = argparse.ArgumentParser(description="Utility to run the solutions to ProjectEuler.")
+parser = argparse.ArgumentParser(description='Utility to run the solutions to ProjectEuler.')
 parser.add_argument('-j', '--jobs', metavar='JOBS', type=check_jobs, default=1, help='Specifies the number of jobs to create.')
 parser.add_argument('-a', '--answers-file', metavar='FILE', help='Specifies the answers file to use to check solutions.')
 parser.add_argument('solutions', metavar='SOLUTION', nargs='+', type=check_solution, default='ALL', help='Specifies the solution(s) to run by problem number. Use ALL to run all existing solutions.')
@@ -36,17 +36,17 @@ if args.answers_file is not None:
     f = open(args.answers_file)
     answers = f.readlines()
 
-solutions_queue = queue.Queue()
+solutions_stack = queue.LifoQueue()
 solutions_count = 0
 results = []
 
-for solution in [f.strip('.cpp') for f in sorted(os.listdir()) if re.match('[0-9]+.cpp', f)] if 'ALL' in args.solutions else args.solutions:
-    solutions_queue.put(solution)
+for solution in [f.strip('.cpp') for f in sorted(os.listdir(), reverse=True) if re.match('[0-9]+.cpp', f)] if 'ALL' in args.solutions else args.solutions:
+    solutions_stack.put(solution)
     solutions_count += 1
 
 def worker():
     while True:
-        solution = solutions_queue.get()
+        solution = solutions_stack.get()
         if solution is None:
             break
 
@@ -79,18 +79,21 @@ def worker():
 
         results.append((solution, int(round(1000 * elapsed, 0)), output, outcome))
         
-        sys.stdout.write("\033[K\r")
+        sys.stdout.write('\033[K\r')
         print('Completed {0} / {1} solutions.'.format(len(results), solutions_count), end='\r')
 
-        solutions_queue.task_done()
+        solutions_stack.task_done()
 
 try:
+    print('Cleaning...')
     subprocess.call(['make', 'clean'], stdout=devnull, stderr=devnull)
 
     global_start = time.perf_counter()
 
+    print('Making utils...')
     subprocess.call(['make', 'utils'], stdout=devnull, stderr=devnull)
 
+    print('Running...')
     jobs = []
     for i in range(args.jobs):
         j = threading.Thread(target=worker)
@@ -98,19 +101,23 @@ try:
         j.start()
         jobs.append(j)
 
-    solutions_queue.join()
-    for i in range(args.jobs):
-        solutions_queue.put(None)
-    for j in jobs:
-        j.join()
+    solutions_stack.join()
 except:
     pass
 finally:
+    print()
+    print('Cleaning up...')
+
+    for i in range(args.jobs):
+        solutions_stack.put(None)
+    for j in jobs:
+        j.join()
+
     global_elapsed = time.perf_counter() - global_start
 
     subprocess.call(['make', 'clean'], stdout=devnull, stderr=devnull)
 
-    sys.stdout.write("\033[K\r")
+    print()
     for result in sorted(results, key=lambda x: x[0]):
         if answers is None:
             print('{0: >4s} | {1: >6d} ms | {2}'.format(*result))
