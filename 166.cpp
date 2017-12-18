5a43f7f577b5d69c53d6bb796c20c073f76284a5
LeeYiyuan <leeyiyuan@hotmail.com>
2017-12-18 15:06:21 +0800

    Improved 166.

--- 166.cpp
+++ 166.cpp
@@ -13,95 +13,132 @@
     in \mathbb{Z}^16, where each element is a digit, to the corresponding 
     augmented matrix
 
-    1   1   1   1   0   0   0   0   0   0   0   0   0   0   0   0   | d
-    0   0   0   0   1   1   1   1   0   0   0   0   0   0   0   0   | d
-    0   0   0   0   0   0   0   0   1   1   1   1   0   0   0   0   | d
-    0   0   0   0   0   0   0   0   0   0   0   0   1   1   1   1   | d
-    1   0   0   0   1   0   0   0   1   0   0   0   1   0   0   0   | d
-    0   1   0   0   0   1   0   0   0   1   0   0   0   1   0   0   | d
-    0   0   1   0   0   0   1   0   0   0   1   0   0   0   1   0   | d
-    0   0   0   1   0   0   0   1   0   0   0   1   0   0   0   1   | d
-    1   0   0   0   0   1   0   0   0   0   1   0   0   0   0   1   | d
-    0   0   0   1   0   0   1   0   0   1   0   0   1   0   0   0   | d
+        1   1   1   1   0   0   0   0   0   0   0   0   0   0   0   0   | d
+        0   0   0   0   1   1   1   1   0   0   0   0   0   0   0   0   | d
+        0   0   0   0   0   0   0   0   1   1   1   1   0   0   0   0   | d
+        0   0   0   0   0   0   0   0   0   0   0   0   1   1   1   1   | d
+        1   0   0   0   1   0   0   0   1   0   0   0   1   0   0   0   | d
+        0   1   0   0   0   1   0   0   0   1   0   0   0   1   0   0   | d
+        0   0   1   0   0   0   1   0   0   0   1   0   0   0   1   0   | d
+        0   0   0   1   0   0   0   1   0   0   0   1   0   0   0   1   | d
+        1   0   0   0   0   1   0   0   0   0   1   0   0   0   0   1   | d
+        0   0   0   1   0   0   1   0   0   1   0   0   1   0   0   0   | d
 
     for which the RREF, using MATLAB, is
 
-    1   0   0   0   0   0   0  -1   0   0   0  -1   0  -1  -1  -1   |  -d
-    0   1   0   0   0   0   0  -1   0   1  -1  -1   0   0  -1  -2   |  -d
-    0   0   1   0   0   0   0   1   0  -1   1   1   0   1   2   2   | 2*d
-    0   0   0   1   0   0   0   1   0   0   0   1   0   0   0   1   |   d
-    0   0   0   0   1   0   0   1   0  -1  -1   0   0   0   0   0   |   0
-    0   0   0   0   0   1   0   1   0   0   1   1   0   1   1   2   | 2*d
-    0   0   0   0   0   0   1  -1   0   1   0  -1   0  -1  -1  -2   |  -d
-    0   0   0   0   0   0   0   0   1   1   1   1   0   0   0   0   |   d
-    0   0   0   0   0   0   0   0   0   0   0   0   1   1   1   1   |   d
-    0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   |   0
+        1   0   0   0   0   0   0  -1   0   0   0  -1   0  -1  -1  -1   |  -d
+        0   1   0   0   0   0   0  -1   0   1  -1  -1   0   0  -1  -2   |  -d
+        0   0   1   0   0   0   0   1   0  -1   1   1   0   1   2   2   | 2*d
+        0   0   0   1   0   0   0   1   0   0   0   1   0   0   0   1   |   d
+        0   0   0   0   1   0   0   1   0  -1  -1   0   0   0   0   0   |   0
+        0   0   0   0   0   1   0   1   0   0   1   1   0   1   1   2   | 2*d
+        0   0   0   0   0   0   1  -1   0   1   0  -1   0  -1  -1  -2   |  -d
+        0   0   0   0   0   0   0   0   1   1   1   1   0   0   0   0   |   d
+        0   0   0   0   0   0   0   0   0   0   0   0   1   1   1   1   |   d
+        0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   |   0
 
     Solving as usual, we get solutions
 
+        d[-1  -1   2   1   0   2   -1  0   1   0   0   0   1   0   0   0 ] + v
 
-    d   [-1  -1   2   1   0   2   -1  0   1   0   0   0   1   0   0   0 ] + 
-    a24 [ 1   1  -1  -1  -1  -1   1   1   0   0   0   0   0   0   0   0 ] + 
-    a32 [ 0  -1   1   0   1   0  -1   0  -1   1   0   0   0   0   0   0 ] + 
-    a33 [ 0   1  -1   0   1  -1   0   0  -1   0   1   0   0   0   0   0 ] +
-    a34 [ 1   1  -1  -1   0  -1   1   0  -1   0   0   1   0   0   0   0 ] +
-    a42 [ 1   0  -1   0   0  -1   1   0   0   0   0   0  -1   1   0   0 ] +
-    a43 [ 1   1  -2   0   0  -1   1   0   0   0   0   0  -1   0   1   0 ] +
-    a44 [ 1   2  -2  -1   0  -2   2   0   0   0   0   0  -1   0   0   1 ] +
+    where v is in the span of the vectors
 
-    where a24, a32, a33, a34, a42, a43, a44 are arbitrary parameters.
+        [ 1   1  -1  -1  -1  -1   1   1   0   0   0   0   0   0   0   0 ],
+        [ 0  -1   1   0   1   0  -1   0  -1   1   0   0   0   0   0   0 ], 
+        [ 0   1  -1   0   1  -1   0   0  -1   0   1   0   0   0   0   0 ],
+        [ 1   1  -1  -1   0  -1   1   0  -1   0   0   1   0   0   0   0 ],
+        [ 1   0  -1   0   0  -1   1   0   0   0   0   0  -1   1   0   0 ],
+        [ 1   1  -2   0   0  -1   1   0   0   0   0   0  -1   0   1   0 ],
+        [ 1   2  -2  -1   0  -2   2   0   0   0   0   0  -1   0   0   1 ]
+    
+    which also has basis, considering the RREF of the appropriate matrix, 
 
-    Finally, we iterate through d = 0, ..., 36 and then run DFS on the free
-    parameters, at each level exploting some monotonic constraints between the
-    free parameters to prune some branches.
+        [ 1   0   0  -1   0   0   0   0   0   2  -2   0  -1  -2   2   1 ],
+        [ 0   1   0  -1   0   0   0   0   0   1  -1   0   0  -2   1   1 ],
+        [ 0   0   1  -1   0   0   0   0   0   1  -1   0   0  -1   0   1 ],
+        [ 0   0   0   0   1   0   0  -1   0   1  -1   0  -1  -1   1   1 ],
+        [ 0   0   0   0   0   1   0  -1   0   0  -1   1   0  -1   1   0 ],
+        [ 0   0   0   0   0   0   1  -1   0  -1   0   1   0   1  -1   0 ],
+        [ 0   0   0   0   0   0   0   0   1   1  -1  -1  -1  -1   1   1 ]
+
+    We loop through each possible value of d from d = 36. For each d, we do
+    DFS search for v using the above basis for the span that v must exist in.
+
+    The nice thing about the form of the above basis is that for each vector,
+    we can easily consider the elements from the current pivot entry to the
+    entry in the column just before the next pivot column to impose a bound
+    on the current coordinate. 
+    
+    Additionally, all the entries from any one pivot entry to the element in
+    the same row just before the next pivot column (or the end of the matrix)
+    is either 1 or -1 which removes the need for any division, which makes 
+    things easier when calculating the required bounds.
 */
 
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <climits>
 
 bool verify(std::vector<int> const &values)
 {
     for (int i = 0; i < values.size(); i++) 
-        if (values[i] < 0 || values[i] > 9) 
-            return false;
+      if (values[i] < 0 || values[i] > 9) 
+        return false;
     return true;
+}
+
+void search(std::vector<std::vector<int>> const &basis, std::vector<int> const &pivot_columns, int index, std::vector<int> const &vector, int &count)
+{
+    int start = INT_MIN, end = INT_MAX;
+
+    for (int i = pivot_columns[index]; i < (index < pivot_columns.size() - 1 ? pivot_columns[index + 1] : vector.size()); i++)
+    {
+        if (basis[index][i] == 1)
+        {
+            start = std::max(start, -vector[i]);
+            end = std::min(end, 9 - vector[i]);
+        }
+        else if (basis[index][i] == -1)
+        {
+            start = std::max(start, vector[i] - 9);
+            end = std::min(end, vector[i]);
+        }
+    }
+
+    if (end < start) return;
+    if (index == basis.size() - 1) { count += end - start + 1; return; }
+
+    std::vector<int> _vector(vector.size());
+    for (int i = start; i <= end; i++)
+    {
+        for (int j = 0; j < _vector.size(); j++)
+            _vector[j] = vector[j] + i * basis[index][j];
+        search(basis, pivot_columns, index + 1, _vector, count);
+    }
 }
 
 int main()
 {
-    std::vector<std::vector<int>> bases = {
-        { -1,  -1,   2,   1,   0,   2,  -1,   0,   1,   0,   0,   0,   1,   0,   0,   0  },
-        {  1,   1,  -1,  -1,  -1,  -1,   1,   1,   0,   0,   0,   0,   0,   0,   0,   0  },
-        {  0,  -1,   1,   0,   1,   0,  -1,   0,  -1,   1,   0,   0,   0,   0,   0,   0  },
-        {  0,   1,  -1,   0,   1,  -1,   0,   0,  -1,   0,   1,   0,   0,   0,   0,   0  },
-        {  1,   1,  -1,  -1,   0,  -1,   1,   0,  -1,   0,   0,   1,   0,   0,   0,   0  },
-        {  1,   0,  -1,   0,   0,  -1,   1,   0,   0,   0,   0,   0,  -1,   1,   0,   0  },
-        {  1,   1,  -2,   0,   0,  -1,   1,   0,   0,   0,   0,   0,  -1,   0,   1,   0  },
-        {  1,   2,  -2,  -1,   0,  -2,   2,   0,   0,   0,   0,   0,  -1,   0,   0,   1  }
+    std::vector<int> d_vector = { -1, -1, 2, 1, 0, 2, -1, 0, 1, 0, 0, 0, 1, 0, 0, 0 };
+    std::vector<std::vector<int>> basis = {
+        { 1,  0,  0, -1,  0,  0,  0,  0,  0,  2, -2,  0, -1, -2,  2, 1 },
+        { 0,  1,  0, -1,  0,  0,  0,  0,  0,  1, -1,  0,  0, -2,  1, 1 },
+        { 0,  0,  1, -1,  0,  0,  0,  0,  0,  1, -1,  0,  0, -1,  0, 1 },
+        { 0,  0,  0,  0,  1,  0,  0, -1,  0,  1, -1,  0, -1, -1,  1, 1 },
+        { 0,  0,  0,  0,  0,  1,  0, -1,  0,  0, -1,  1,  0, -1,  1, 0 },
+        { 0,  0,  0,  0,  0,  0,  1, -1,  0, -1,  0,  1,  0,  1, -1, 0 },
+        { 0,  0,  0,  0,  0,  0,  0,  0,  1,  1, -1, -1, -1, -1,  1, 1 }
     };
+    std::vector<int> pivot_columns = { 0, 1, 2, 4, 5, 6, 8 };
 
     int count = 0;
     std::vector<int> vector(16);
     for (int d = 0; d <= 36; d++)
     {
-        for (int a24 = 0; a24 <= std::min(9, d); a24++)
-            for (int a32 = 0; a32 <= std::min(9, d); a32++)
-                for (int a33 = 0; a33 <= std::min(9, std::min(9 + a24 - a32, std::min(2 * d - a24, d - a32))); a33++)
-                    for (int a34 = 0; a34 <= std::min(9, std::min(9 + d - a24 - a34, std::min(d - a24, std::min(2 * d - a24 - a33, d - a32 - a33)))); a34++)
-                        for (int a42 = 0; a42 <= std::min(9, std::min(2 * d - a24 - a33 - a34, d)); a42++)
-                            for (int a43 = 0; a43 <= std::min(9, std::min(2 * d - a24 - a33 - a34 - a42, d - a42)); a43++)
-                                for (int a44 = 0; a44 <= std::min(9, std::min(9 + d - a24 - a34 - a42 - a43, std::min(d - a24 - a34, std::min((2 * d - a24 - a33 - a34 - a42 - a43) / 2, d - a42 - a43)))); a44++, count += verify(vector))
-                                    for (int i = 0; i < 16; i++)
-                                        vector[i] = 
-                                            d   * bases[0][i] + 
-                                            a24 * bases[1][i] + 
-                                            a32 * bases[2][i] +
-                                            a33 * bases[3][i] +
-                                            a34 * bases[4][i] +
-                                            a42 * bases[5][i] +
-                                            a43 * bases[6][i] +
-                                            a44 * bases[7][i];
+        search(basis, pivot_columns, 0, vector, count);
+        for (int i = 0; i < vector.size(); i++)
+            vector[i] += d_vector[i];
     }
 
     std::cout << count << std::endl;
